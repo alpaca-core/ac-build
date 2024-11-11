272c15e4a9b29fb2fa30ace5b6340f356c157076
Borislav Stanimirov <b@ibob.bg>
2024-11-11 09:20:47 +0200

    refactor: infer standalone

--- init_ac_local_prj.cmake
+++ init_ac_local_prj.cmake
@@ -3,21 +3,24 @@
 #
 set(AC_LOCAL_CMAKE_LIB_DIR "${CMAKE_CURRENT_LIST_DIR}")
 
-option(AC_STANDALONE "project is standalone" ON)
 option(AC_MONO_ROOT "project is a monorepo root" OFF)
 option(AC_MONO_COMP "project is a monorepo component" OFF)
 option(AC_DEPLOY_ROOT "project is a deploy root" OFF)
 option(AC_DEPLOY_COMP "project is a deploy component" OFF)
-mark_as_advanced(AC_STANDALONE AC_MONO_ROOT AC_MONO_COMP AC_DEPLOY_ROOT AC_DEPLOY_COMP)
+mark_as_advanced(AC_MONO_ROOT AC_MONO_COMP AC_DEPLOY_ROOT AC_DEPLOY_COMP)
 
 if(CMAKE_SOURCE_DIR STREQUAL CMAKE_CURRENT_SOURCE_DIR)
     set(projectIsSubdir NO)
     set(projectIsRoot YES)
-    set(projectIsStandalone ${AC_STANDALONE})
     set(projectIsMonoRoot ${AC_MONO_ROOT})
     set(projectIsMonoComponent ${AC_MONO_COMP})
     set(projectIsDeployRoot ${AC_DEPLOY_ROOT})
     set(projectIsDeployComponent ${AC_DEPLOY_COMP})
+    if(NOT projectIsMonoRoot AND NOT projecdIsMonoComponent
+        AND NOT projectIsDeployRoot AND NOT projectIsDeployComponent)
+        # standalone if none of the above opinions are set
+        set(projectIsStandalone YES)
+    endif()
     include(setup_ac_local_prj_root)
 else()
     set(projectIsSubdir YES)
